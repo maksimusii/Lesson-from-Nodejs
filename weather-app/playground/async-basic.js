3395f6f0c88b873656cdf57b1d1e3e79096725f6
Maksim Karpov <maksimus@rtural.ru>
2019-09-06 23:37:22 +0500

    remove app-w

--- weather-app/playground/async-basic.js
+++
@@ -1,8 +0,0 @@
-console.log('Starting app');
-setTimeout(() => {
-    console.log('Inside of callback');
-    }, 2000);
-  setTimeout(() =>  {
-      console.log('Second setTimeout')
-},3000);
-console.log('Finishing up');
